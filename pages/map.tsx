0a09d252e5e65b9c7311d10c6cbd9100845d0a0b
Benjamin TD <benjamin.tdm@gmail.com>
2020-03-26 23:04:07 +0100

    height on mobile

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default () => (
-  <div className="w-screen h-screen">
-    <iframe
-      width="100%"
-      height="100%"
-      src="https://co-confines.ushahidi.io/views/map"
-      frameBorder={0}
-      allowFullScreen={true}
-    ></iframe>
-  </div>
-);
+export default () => {
+  const [height, setHeight] = useState("100vh" as any);
+  useEffect(() => {
+    setHeight(window.innerHeight);
+  }, []);
+
+  return (
+    <div className="w-screen" style={{ height }}>
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://co-confines.ushahidi.io/views/map"
+        frameBorder={0}
+        allowFullScreen={true}
+      ></iframe>
+    </div>
+  );
+};
